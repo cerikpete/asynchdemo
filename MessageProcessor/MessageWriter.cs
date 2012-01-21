0c2fa0a79d8e6f7cf1ba9e58eb1deb8b0390181d
Erik Peterson <cerikpete@gmail.com>
2012-01-21 15:04:38 -0500

    Initial commit

---
+++ MessageProcessor/MessageWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MessageProcessor
+{
+    class MessageWriter
+    {
+        public void WriteMessage(string message, int sleepSeconds = 0)
+        {
+            if (sleepSeconds > 0)
+                Thread.Sleep(sleepSeconds * 1000);
+
+            Console.WriteLine(message);
+        }
+    }    
+}
---
+++ MessageProcessor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entering the processor");
+
+            var task1 = Task.Factory.StartNew(() =>
+            {
+                var writer = new MessageWriter();
+                writer.WriteMessage("This is the first message", 5);
+            });
+
+            var task2 = Task.Factory.StartNew(() =>
+            {
+                var writer = new MessageWriter();
+                writer.WriteMessage("This is the second message");
+            });
+
+            Task.WaitAll(task1, task2);
+
+            Console.WriteLine("Completed all of the tasks");
+            Console.ReadKey();
+        }
+    }
+}
